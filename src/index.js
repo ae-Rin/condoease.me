fb3ddbc65c30124454d737ca5ab935df1f54721a
aerin <164000281+ae-Rin@users.noreply.github.com>
2025-06-28 13:58:28 +0800

    deleted mobile import

--- src/index.js
+++ src/index.js
@@ -4,17 +4,14 @@ import { Provider } from 'react-redux'
 import 'core-js'
 
 import WebApp from './web/WebApp' // Use WebApp as the entry point
-import MobileApp from './mobile/MobileApp' // Import MobileApp if needed
 import store from './store'
 import { UserProvider } from './context/UserContext' // Import UserProvider
 
-const isMobile = import.meta.env.VITE_PLATFORM === 'mobile'
-console.log('VITE_PLATFORM:', import.meta.env.VITE_PLATFORM)
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <UserProvider>{isMobile ? <MobileApp /> : <WebApp />}</UserProvider>
+      <UserProvider>{<WebApp />}</UserProvider>
     </Provider>
   </React.StrictMode>,
 )
